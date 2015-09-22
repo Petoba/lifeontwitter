fc9f06cf03321df2e5c0bbff7f0210a674236ccf
ddarrko <daniel@10yetis.co.uk>
2015-09-22 19:17:38 +0100

    remove secret codes etc
    really just removing my twitter app tokens and the boring static
    generated html files

--- includes/config.php
+++ includes/config.php
@@ -6,15 +6,15 @@ error_reporting(0);
 class Config
 {
     const DB_SERVER     = 'localhost';
-    const DB_NAME       = 'yearontwitter';
-    const DB_USERNAME   = 'yearontwitter';
-    const DB_PASSWORD   = 'Yet1l1v3s';
+    const DB_NAME       = '';
+    const DB_USERNAME   = '';
+    const DB_PASSWORD   = '';
 
     const SITE_NAME     = 'Life on Twitter';
 
-    const CONSUMER_KEY  = 'XZRpvnCUQ7jQkKwMj9FYMnzgi';
-    const CONSUMER_SEC  = 'WzfUeJiqypZZoYpgkjwnnMOakuYspOIML04x3bC7713ehZ3Dek';
-    const REDIRECT      = 'http://lifeontwitter.com/generate';
+    const CONSUMER_KEY  = 'xxx';
+    const CONSUMER_SEC  = 'xxx';
+    const REDIRECT      = 'redirurl';
 
     const BASE_URL      = 'http://lifeontwitter.com/';
 }
